3536bfe02e5c00c006584369739bf2d3664a5551
audreyandoy <audreylandoy@gmail.com>
2019-04-24 17:08:17 -0700

    removed materialize npm package had security risk

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.static(__dirname + '/static/'));
 app.use(session({
   secret: process.env.SESSION_SECRET,
 	resave: false,
-  saveUninitialized: true
+  saveUninitialized: true,
 }));
 
 app.use(flash());
